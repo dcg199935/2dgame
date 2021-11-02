1d86a32d8a44266f4ef1eb6ca8db70d977bdf37e
dcg199935 <54872723+dcg199935@users.noreply.github.com>
2021-11-02 15:20:53 -0400

    first

---
+++ New Unity Project/Assets/code/NewBehaviourScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int speed = 5;
+    int jumpForce = 800;
+    Rigidbody2D _rigidbody;
+    Animator _animator;
+
+    public Transform feet;
+    public LayerMask ground;
+    public bool isGrounded = false;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       float xSpeed = Input.GetAxis("Horizontal") * speed; 
+       _rigidbody.velocity = new Vector2 (xSpeed, _rigidbody.velocity.y);
+       _animator.SetFloat("Speed", Mathf.Abs(xSpeed));
+    }
+
+    private void Update()
+    {    
+         isGrounded = Physics2D.OverlapCircle(feet.position,.3f, ground);
+
+         if (Input.GetButtonDown("Jump") && isGrounded)
+         {
+             _rigidbody.AddForce(new Vector2(0, jumpForce));
+         }
+    }
+}
